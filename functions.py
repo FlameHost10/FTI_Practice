706d9341ee7378b0ecce1c41424e9e1b53f40519
Bushlya Fedor <fedor10401@mail.ru>
2024-04-14 17:06:08 +0300

    earth block

--- functions.py
+++ functions.py
@@ -1,11 +1,8 @@
-import math
-
-from skyfield.api import load
+from math import sin, cos, asin, acos, radians
 from skyfield.sgp4lib import EarthSatellite
-from skyfield.api import Angle, wgs84, Topos
 
 
-def time_coordinates_cubesat(required_time, address):
+def time_coordinates_cubesat(utc_time, address):
     min_time_diff = float("inf")
     tle_data = ["", ""]
 
@@ -17,38 +14,44 @@ def time_coordinates_cubesat(required_time, address):
         line_second = tle_list[i + 2].rstrip()
 
         satellite = EarthSatellite(line_first, line_second)
-        if abs(required_time - satellite.epoch.tt) < min_time_diff:
-            min_time_diff = abs(required_time - satellite.epoch.tt)
+
+        if abs(utc_time.tt - satellite.epoch.tt) < min_time_diff:
+            min_time_diff = abs(utc_time.tt - satellite.epoch.tt)
             tle_data[0] = line_first
             tle_data[1] = line_second
 
     satellite = EarthSatellite(tle_data[0], tle_data[1])
+    geocentric = satellite.at(utc_time)
+    ra, dec, r = geocentric.radec()
+    print(ra, dec, r)
 
-    ts = load.timescale()
-    t = ts.now()
+    print('Time of interest: ', utc_time.utc_jpl())
+    print('Satellite position: ', ra._degrees, dec._degrees, r.km)
+    print('Satellite epoch: ', satellite.epoch.utc_jpl())
 
-    topocentric = Topos(latitude_degrees=51.48, longitude_degrees=-0.19)
-
-    difference = satellite - topocentric
-    topocentric = difference.at(t)
-    alt, az, distance = topocentric.altaz()
-
-    ra = az.radians
-    dec = alt.radians
-
-    return math.degrees(ra), math.degrees(dec)
+    return ra._degrees, dec._degrees, r.km
 
 
-def blackout_cubesat(satellite_ra, satellite_dec, light_source_ra, light_source_dec):
-    ts = load.timescale()
+def earth_block_degrees(sc_ra, sc_dec, sc_r, light_source_ra, light_source_dec, light_source_r):
+    sc_ra = radians(sc_ra)
+    sc_dec = radians(sc_dec)
 
-    observer_location = Topos(latitude_degrees=light_source_dec, longitude_degrees=light_source_ra)
+    light_source_ra = radians(light_source_ra)
+    light_source_dec = radians(light_source_dec)
 
-    satellite_position = Topos(latitude_degrees=satellite_dec, longitude_degrees=satellite_ra)
+    r_Earth = 6371  # km
 
-    t = ts.now()
-    difference = satellite_position - observer_location
-    topocentric = difference.at(t)
-    alt, az, distance = topocentric.altaz()
+    sc_z = sc_r * sin(sc_dec)
+    sc_y = sc_r * cos(sc_dec) * sin(sc_ra)
+    sc_x = sc_r * cos(sc_dec) * cos(sc_ra)
 
-    return alt.degrees > 0
+    light_source_z = light_source_r * sin(light_source_dec)
+    light_source_y = light_source_r * cos(light_source_dec) * sin(light_source_ra)
+    light_source_x = light_source_r * cos(light_source_dec) * cos(light_source_ra)
+
+    Theta_Earth = asin(r_Earth / sc_r)
+
+    dot_sc_light_source = sc_x * light_source_x + sc_y * light_source_y + sc_z * light_source_z
+    Theta_src = acos(-dot_sc_light_source / (sc_r * light_source_r))
+
+    return Theta_Earth > Theta_src
--- main.py
+++ main.py
@@ -1,49 +1,16 @@
-from datetime import datetime
-
+from functions import *
 from skyfield.api import load
 
-from functions import *
+if __name__ == '__main__':
+    address_array = ["resources/202307_GRBAlpha_tle.txt", "resources/202307_Monitor-2_tle.txt",
+                     "resources/202307_Monitor-3_tle.txt", "resources/202307_Monitor-4_tle.txt",
+                     "resources/202307_VZLUSAT-2_tle.txt"]
 
-#
-# def data_processing(address):
-#     name = address.split("/")[-1].split(".")[0]
-#     text_file = open("resources/results/" + name + ".txt", "w")
-#     with open(address) as reader:
-#         tle_list = reader.readlines()
-#
-#     for i in range(0, len(tle_list), 4):
-#         line_first = tle_list[i].rstrip()
-#         line_second = tle_list[i + 2].rstrip()
-#
-#         satellite = EarthSatellite(line_first, line_second, name, ts)
-#         geocentric = satellite.at(satellite.epoch)
-#
-#         text_file.write(str(satellite) + "\n")
-#         text_file.write(str(geocentric.position.km) + "\n")
-#         text_file.write("in sunlight\n" if geocentric.is_sunlit(eph) else "in shadow\n")
-#         text_file.write("\n")
-#
-#
-# def preparatory_function(address_array):
-#     if not os.path.exists("resources/results"):
-#         os.mkdir("resources/results")
-#     for address in address_array:
-#         data_processing(address)
+    ts = load.timescale()
+    utc_time = ts.utc(2023, 7, 1)
 
+    print(time_coordinates_cubesat(utc_time, address_array[0]))
 
-address_array = ["resources/202307_GRBAlpha_tle.txt", "resources/202307_Monitor-2_tle.txt",
-                 "resources/202307_Monitor-3_tle.txt", "resources/202307_Monitor-4_tle.txt",
-                 "resources/202307_VZLUSAT-2_tle.txt"]
-
-
-ts = load.timescale()
-utc_time = ts.utc(2023, 7, 1)
-epoch_time = utc_time.tt
-
-print(time_coordinates_cubesat(epoch_time, address_array[0]))
-
-ra, dec = time_coordinates_cubesat(epoch_time, address_array[0])
-print(blackout_cubesat(ra, dec, 180, 0))
-
-
-
+    print(earth_block_degrees(0, 90, 10000, 180, -90, 10000))
+    print(earth_block_degrees(90, 0, 10000, 270, 0, 10000))
+    print(earth_block_degrees(0, 90, 10000, 0, 90, 10001))
